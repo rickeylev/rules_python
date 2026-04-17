6197b32cae48ce73ab0aa70aa8a89724efd32dbc
Richard Levasseur <rlevasseur@google.com>
2026-04-17 15:10:39 -0700

    make test check for module from zip

--- tests/tools/zip_stdlib/stdlib_imported_from_zip_test.py
+++ tests/tools/zip_stdlib/stdlib_imported_from_zip_test.py
@@ -1,15 +1,16 @@
 import unittest
 import pathlib
 import os
+import sys
 
-class BasicTest(unittest.TestCase):
+class StdlibImportedFromZipTest(unittest.TestCase):
     def test_imports_work(self):
         self.assertTrue(hasattr(pathlib, 'Path'))
         self.assertTrue(hasattr(os, 'path'))
         
         pathlib_file = pathlib.__file__
-        print(f"pathlib is loaded from: {pathlib_file}")
-        self.assertIn(".zip", pathlib_file, "pathlib was not loaded from a zip file.")
+        
+        self.assertIn(".zip", pathlib_file, f"pathlib was not loaded from a zip file: {pathlib_file}")
 
 if __name__ == "__main__":
     unittest.main()
