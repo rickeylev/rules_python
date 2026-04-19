c830949a04416c9da589de76cf3466a8ae824c38
Richard Levasseur <richardlev@gmail.com>
2026-04-19 09:19:25 -0700

    make system_python_zipapp_test pass

--- tests/py_zipapp/system_python_zipapp_test.py
+++ tests/py_zipapp/system_python_zipapp_test.py
@@ -11,7 +11,16 @@ class SystemPythonZipAppTest(unittest.TestCase):
         self.assertTrue(os.path.exists(zipapp_path))
         self.assertTrue(os.path.isfile(zipapp_path))
 
-        output = subprocess.check_output([zipapp_path]).decode("utf-8").strip()
+        try:
+            output = subprocess.check_output([zipapp_path], stderr=subprocess.STDOUT).decode("utf-8").strip()
+        except subprocess.CalledProcessError as e:
+            self.fail(
+                "exit code: {}\n"
+                " command: {}\n"
+                "===== stdout/stderr start ==={}===== stdout/stderr end ====".format(
+                    e.returncode, e.cmd, e.output.decode("utf-8")
+                )
+            )
         self.assertIn("Hello from zipapp", output)
         self.assertIn("dep:", output)
 
